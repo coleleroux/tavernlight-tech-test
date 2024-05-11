5b00a2a223474e997f94571c6f80a4f7089ed03d
Colman L (SquidSync) <coleleroux@gmail.com>
2024-05-11 19:56:53 -0400

    Q4 fix- Delete any dynamically added Player

--- Q4.cpp
+++ Q4.cpp
@@ -1,14 +1,21 @@
 void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId) {
     Player* player = g_game.getPlayerByName(recipient);
+    bool isNewPlayer = false;
+
     if (!player) {
         player = new Player(nullptr);
+        isNewPlayer = true;
         if (!IOLoginData::loadPlayerByName(player, recipient)) {
+            delete player; // Delete the newly created player if loading fails
             return;
         }
     }
 
     Item* item = Item::CreateItem(itemId);
     if (!item) {
+        if (isNewPlayer) {
+            delete player; // Delete the newly created player if item creation fails
+        }
         return;
     }
 
@@ -16,5 +23,9 @@ void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId) {
 
     if (player->isOffline()) {
         IOLoginData::savePlayer(player);
+    }
+
+    if (isNewPlayer) {
+        delete player; // Delete the newly created player after use if it was created in this method
     }
 }
